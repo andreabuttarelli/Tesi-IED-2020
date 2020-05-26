6568dad1f08f53596301e2695bca4059ee9a9be3
andreabuttarelli <andreabuttarelli.com>
2020-05-26 12:31:27 +0200

    26/05/2020 - Restart

--- dev/app/lib/src/blocs/authentication/index.dart
+++
@@ -1,8 +0,0 @@
-/// MIT License
-/// by Andrea Buttarelli
-/// creato il 04/02/2020
-
-export 'bloc.dart';
-export 'event.dart';
-export 'state.dart';
-export 'package:app/src/repositories/user.dart';
